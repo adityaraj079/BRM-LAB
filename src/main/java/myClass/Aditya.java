410da239c0194da6426d88390abf289210394359
Aditya <rajaditya079@gmail.com>
2021-02-14 20:51:51 +0530

    New is lab 2

---
+++ src/main/java/myClass/Aditya.java
@@ -0,0 +1,11 @@
+package myClass;
+
+public class Aditya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This on is for jenkins");
+
+	}
+
+}
